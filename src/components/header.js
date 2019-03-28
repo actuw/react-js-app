af6bab9d308b81b53f751e72f7fdad34a36af31b
actuw <duncanpiscopo@gmail.com>
2019-03-28 23:39:32 +0100

    started working on my first react app

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React,{ Component } from 'react';
+
+//const Component = React.Component;
+
+class Header extends Component{
+    render(){
+
+        return (
+            <header>
+                <div className="logo">Logo</div>
+                <input type='text'/>
+            </header>
+        )
+    }
+}
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'; //calling react from node_modules
+import ReactDOM from 'react-dom'; //calling react-dom from node_modules
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//components to be included inside of parent/index
+import Header from './components/header'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const App = () =>{
+    return (
+        <div className="newElement">
+            <Header/>
+        </div>) 
+    // behind the scenes, react is doing these commandsReact.createElement('h1',{className:'title'},React.createElement('div'));
+
+} // instatiating header component and assigning css class
+
+ReactDOM.render(<App/>, document.querySelector('#root')) //injecting component into dom markup by id
